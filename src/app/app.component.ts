3cb294326a7974708f077346b1914adbc43522e0
ivonaldo <ivonaldo.soares@am4.com.br>
2019-07-01 21:43:56 -0300

    todolist simple in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'avanade';
+export class AppComponent implements OnInit {
+  title = 'Ivonaldo';
+  sobrenome = '';
+
+  items = ['1', '4', '5', '6', '12', '15'];
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.sobrenome = 'Soares';
+    }, 2000);
+  };
+  removeItem(index) {
+    this.items.splice(index, 1);
+  }
+  addTodo(todo: string) {
+    this.items.push(todo);
+  }
 }
